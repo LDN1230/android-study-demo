19d9959200fdc19f8f390586f24b11ff21f3eb82
Danny Lin <32321924+LDN1230@users.noreply.github.com>
2020-06-02 22:59:37 +0800

    编写Fragment1及其相关资源

--- KingOfHonor/app/src/main/java/ldn/KingOfHonor/fragment/Fragment1.java
+++ KingOfHonor/app/src/main/java/ldn/KingOfHonor/fragment/Fragment1.java
@@ -1,6 +1,18 @@
 package ldn.KingOfHonor.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import org.greenrobot.eventbus.EventBus;
+
+import ldn.KingOfHonor.R;
+import ldn.KingOfHonor.sqlite.HeroSQLiteHelper;
 
 /**
  * @author: LDN
@@ -8,4 +20,17 @@ import androidx.fragment.app.Fragment;
  */
 public class Fragment1 extends Fragment {
 
+    private View view; //该Fragment的布局
+    private HeroSQLiteHelper mySQLiteHelper;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // 注册 EventBus
+        EventBus.getDefault().register(this);
+        view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment1, container, false);
+        mySQLiteHelper= new HeroSQLiteHelper(getContext());
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
